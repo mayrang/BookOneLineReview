de2291f1950a62fe3115357f0fd137cef81cd890
mayrang <pkss0626@naver.com>
2022-04-10 23:07:43 +0900

    make_ReviewPage1

--- src/Review.js
+++ src/Review.js
@@ -1,8 +1,26 @@
-
+import { useNavigate, useParams } from "react-router-dom";
+import { useState, useEffect, useContext } from "react";
+import { ReviewStateContext} from "./App.js";
 
 const Review = () => {
+    const [data, setData] = useState();
+    const navigate = useNavigate();
+    const {id} = useParams();
+    const reviewList = useContext(ReviewStateContext);
+    useEffect(() => {
+        const targetData = reviewList.find((it) => parseInt(it.id) == parseInt(id));
+        if(targetData){
+            setData(targetData);
+        }else{
+            alert("해당되는 페이지가 없습니다.");
+            navigate(-1, {replace:True});
+        }
+        
+    }, [reviewList]);
     return (
-        <div></div>
+        <div className="ReviewPage">
+
+        </div>
     );
 }
 
